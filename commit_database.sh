ab711b9183e0930e1fc1cbfaeec10166a1346bcd
austinletson <waustinletson@gmail.com>
2025-03-13 07:08:04 -0400

    Updating SorryDB at 2025-03-13 07:08:04

--- commit_database.sh
+++ commit_database.sh
@@ -17,7 +17,7 @@ echo "Updating database..."
 # Run the Docker container with the mounted volume to update databse
 docker run --rm \
   --user $(id -u):$(id -g) \
-  -v /home/austin/development/lean/sorry-index/sorry-db-data-test-mock-only:/data \
+  --mount type=bind,source=/home/austin/development/lean/sorry-index/sorry-db-data-test-mock-only,target=/data \
   sorrydb:latest \
   poetry run update_db --database-file /data/sorry_database.json
 
