f26a6770e1fb999f4115cd64f9e1dcae0fd4a7c3
austinletson <waustinletson@gmail.com>
2025-04-07 08:36:50 -0400

    Updating SorryDB at 2025-04-07 08:33:41

--- commit_database.sh
+++ commit_database.sh
@@ -23,7 +23,7 @@ echo "Updating database..."
 docker run \
   --mount type=bind,source=/home/austin/development/lean/sorry-index/sorry-db-data-test-mock-only,target=/data \
   sorrydb-mathlib-diff:latest \
-  poetry run update_db --database-file /data/sorry_database.json --stats-file /data/update_database_stats.json --log-file "/data/logs/${LOG_FILE}"
+  poetry run update_db --database-file /data/sorry_database.json --stats-file /data/update_database_stats.json --log-file "/data/logs/${LOG_FILE}" --log-level DEBUG
 
 
 echo "Staging changes..."
